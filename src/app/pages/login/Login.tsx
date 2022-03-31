3a1d261e95a2c2194996852de8748d56d0f9338e
Roger-Franco <rogerfrancogalt@gmail.com>
2022-03-31 18:17:27 -0400

    useState

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,15 +1,30 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export const Login = () => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate('/pagina-inicial')
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+  // const navigate = useNavigate();
+  // const handleClick = () => {
+  //   navigate('/pagina-inicial')
+  // }
+  const handleEntrar = () => {
+    console.log(email);
+    console.log(senha);
   }
   return (
     <div>
-      Login
-      <br />
-      <button onClick={handleClick}>Página Inicial</button>
+      <form action="">
+        <label htmlFor="">
+          <span>Email:</span>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label htmlFor="">
+          <span>Senha:</span>
+          <input value={senha} onChange={(e) => setSenha(e.target.value)} type="password" />
+        </label>
+        <button onClick={handleEntrar} type='button'>Entrar</button>
+      </form>
     </div >
   )
 }

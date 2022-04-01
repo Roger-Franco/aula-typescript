91d87a5cb9204934f8598e293c8664c08fd18c59
Roger-Franco <rogerfrancogalt@gmail.com>
2022-04-01 17:42:25 -0400

    useRef

--- README-install.md
+++ README-install.md
@@ -4,4 +4,4 @@ https://www.youtube.com/watch?v=1bEbBkWc4-I&list=PL29TaWXah3iZktD5o1IHbc7JDqG_80
 
 Aula atual:
 
-https://www.youtube.com/watch?v=Uli4R2PKnxA&list=PL29TaWXah3iZktD5o1IHbc7JDqG_80iOm&index=10
+https://www.youtube.com/watch?v=ZPNUCOtyCDM&list=PL29TaWXah3iZktD5o1IHbc7JDqG_80iOm&index=11
--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,7 +1,9 @@
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useMemo, useRef, useState } from 'react'
 // import { useNavigate } from 'react-router-dom'
 
 export const Login = () => {
+  const inputPasswordRef = useRef<HTMLInputElement>(null)
+
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
 
@@ -18,6 +20,11 @@ export const Login = () => {
   const handleEntrar = useCallback(() => {
     console.log(email);
     console.log(senha);
+
+    if (inputPasswordRef.current !== null) {
+      inputPasswordRef.current.focus()
+    }
+
   }, [email, senha]) // com o useCallback, as funcões dentro dela não são reconstruídas até que as dependencias serem chamadas;
   // const handleEntrar = () => {
   //   console.log(email);
@@ -29,11 +36,11 @@ export const Login = () => {
         <p>Quantidade de caracteres no email: {emailLength}</p>
         <label htmlFor="">
           <span>Email:</span>
-          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined} value={email} onChange={(e) => setEmail(e.target.value)} />
         </label>
         <label htmlFor="">
           <span>Senha:</span>
-          <input value={senha} onChange={(e) => setSenha(e.target.value)} type="password" />
+          <input ref={inputPasswordRef} value={senha} onChange={(e) => setSenha(e.target.value)} type="password" />
         </label>
         <button onClick={handleEntrar} type='button'>Entrar</button>
       </form>

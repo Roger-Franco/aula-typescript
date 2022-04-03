376b6077a335948a1215a3ce8a46a98ef5f1c767
Roger-Franco <rogerfrancogalt@gmail.com>
2022-04-03 16:48:49 -0400

    Trabalhando com listas

--- README-install.md
+++ README-install.md
@@ -4,4 +4,4 @@ https://www.youtube.com/watch?v=1bEbBkWc4-I&list=PL29TaWXah3iZktD5o1IHbc7JDqG_80
 
 Aula atual:
 
-https://www.youtube.com/watch?v=p2V-iWxgVKE&list=PL29TaWXah3iZktD5o1IHbc7JDqG_80iOm&index=20
+https://www.youtube.com/watch?v=YMxc9biU9ms&list=PL29TaWXah3iZktD5o1IHbc7JDqG_80iOm&index=21
--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,26 +1,37 @@
-import { useContext, useRef } from 'react'
-import { Link } from 'react-router-dom'
-// import { UsuarioLogadoContext } from '../../shared/contexts'
-import { useUsuarioLogado } from '../../shared/hooks'
+import { useCallback, useState } from 'react'
 
 export const Dashboard = () => {
-  const counterRef = useRef({ counter: 0 })
+  const [lista, setLista] = useState<string[]>(['lista1', 'lista2'])
+  const [nome, setNome] = useState<string[]>(['lista1', 'lista2', 'qualquer', 'teste'])
 
-  const { nomeDoUsuario, logOut } = useUsuarioLogado()
-  // Usando aqui um react hook customizado 
+  const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+    if (e.key === 'Enter') {
+      if (e.currentTarget.value.trim().length === 0) return;
 
-  // const usuarioLogadoContext = useContext(UsuarioLogadoContext)
+      // setLista([...lista, e.currentTarget.value]) // ou mais correto - nesse caso precisa 
+      // do lista em dependencias no useEffect
+      const value = e.currentTarget.value
+
+      e.currentTarget.value = ''
+
+      setLista((oldlista) => {
+
+        if (oldlista.includes(value)) return oldlista;
+
+        return [...oldlista, value]
+      })
+    }
+  }, [])
 
   return (
-    <>
-      <h1>Dashboard</h1>
-      <p>{nomeDoUsuario}</p>
-      <p>Contador: {counterRef.current.counter}</p>
-
-      <button onClick={() => counterRef.current.counter++}>Somar</button>
-      <button onClick={() => console.log(counterRef.current.counter)}>Somar</button>
-      <button onClick={logOut}>logOut</button>
-      <Link to="/entrar" >Login</Link>
-    </>
+    <div>
+      <p>Lista</p>
+      <input onKeyDown={handleInputKeyDown} />
+      <ul>
+        {lista.map((value) => {
+          return <li key={value}>{value}</li>
+        })}
+      </ul>
+    </div>
   )
 }
